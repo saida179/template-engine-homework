e4a2716bf76f18aa3a2dbaf2d62f935fe024e4f1
saida179 <69127945+saida179@users.noreply.github.com>
2020-10-22 00:03:18 -0700

    trying to get my application to work

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "main.html");
+const render = require("./lib/htmlRenderer");
 const employeeArr = [];
 
 
 function addWorkers() {
     inquirer.prompt([{
         type: "list",
-        message: "job title ",
+        message: "Job Title ",
         choices: ["Manager", "Engineer", "Intern"]
     },
     {
 
         type: "input",
-        name: "new employee name",
-        message: "Enter employee's name:",
+        name: "New employee name",
+        message: "Enter employee's Name:",
     },
     {
         type: "input",
@@ -29,6 +31,23 @@ function addWorkers() {
     {
         type: "input",
         name: "Email",
-        message: "Enter employee's email:",
+        message: "Enter employee's Email:",
     },
 ])
+    .then(function (answers) {
+        if (answers.Choose === "Manager") {
+            inquirer.prompt([
+                {
+                    
+                    type: "input",
+                    name: "?",
+                    message: "?",
+                }
+            ])
+            .then(function (res) {
+                const engineer = new Engineer(answers.name, answers.id, answers.email, res.github);
+                employees.push(engineer);
+            })
+    
+}
+//I hit a wall.. I'm confused from where to go from here theres something that I wanted to try out but heres no more time
